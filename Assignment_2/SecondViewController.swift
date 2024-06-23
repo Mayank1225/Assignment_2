8fbc5130075acf862ec0f0e64defa0fdca01b8fb
Mayank1225 <mayankmalviya2169@gmail.com>
2024-06-23 16:00:16 -0700

    add some change

--- Assignment_2/SecondViewController.swift
+++ Assignment_2/SecondViewController.swift
@@ -11,7 +11,7 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
         let price: String
     }
     
-    let data: [Datalist] = [
+    var data: [Datalist] = [
         Datalist(Name: "Dell", ImgName: "img1", detail: "RAM 4GB", price: "$299.99"),
         Datalist(Name: "Hp", ImgName: "img2", detail: "RAM 8GB", price: "$499.99"),
         Datalist(Name: "Lenovo", ImgName: "img3", detail: "RAM 8GB", price: "$179.99"),
@@ -22,25 +22,39 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Laptops";
-        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil);
-        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: nil);
+        
+        self.navigationItem.hidesBackButton = true
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItemInDataList));
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(backToMain));
         table.dataSource = self;
         table.delegate = self;
-        
-           let cellNib = UINib(nibName: "SpecialTableViewCell", bundle: nil)
-           table.register(cellNib, forCellReuseIdentifier: "specialcell")
+    }
+                                                                
+        @objc func backToMain() {
+            self.navigationController?.popViewController(animated: true)
+        }
+                                                                
+    @objc func addItemInDataList() {
+        // Define the new item
+        let newItem = Datalist(Name: "IBM TABLETS", ImgName: "img1", detail: "Id: 9001415", price: "$999.99")
+        data.insert(newItem, at: getFifthNumber(from: "9001415")! - 1)
+        table.reloadData()
+    }
+    
+    func getFifthNumber(from string: String) -> Int? {
+        guard string.count > 4 else {
+            return nil
+        }
+        let index = string.index(string.startIndex, offsetBy: 4)
+        return Int(String(string[index]));
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.row == 3{
-//            let specialcell = table.dequeueReusableCell(withIdentifier: "specialcell", for: indexPath) as! SpecialTableViewCell
-//            specialcell.studentID.text = "IBM TABLET (9001415): $900.00";
-//                                      
-//            return specialcell;
-//        } else {
+
             let row = data[indexPath.row]
             let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomeTableViewCell
             cell.NameOfProduct.text = row.Name
@@ -49,7 +63,6 @@ class SecondViewController: UIViewController, UITableViewDataSource, UITableView
             cell.DetailOfProduct.text = row.detail;
             
             return cell;
-    
         
     }
     
